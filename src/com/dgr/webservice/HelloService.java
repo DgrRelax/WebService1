4dfcec3b377db44f0c7bcffc733efbe85b6240c1
DgrRelax <13723762475@163.com>
2018-09-20 11:28:54 +0800

    WebService

---
+++ src/com/dgr/webservice/HelloService.java
@@ -0,0 +1,8 @@
+package com.dgr.webservice;
+
+public class HelloService {
+	public String say(String name){
+		return "Hello " + name;
+	}
+	//http://localhost:8080/WebService1/services/HelloService?wsdl
+}
